646536fc52de226c97683a7e511ab181124a37eb
Tobias Nyholm <tobias.nyholm@gmail.com>
2020-02-14 13:41:31 +0100

    Drop SF3.4 to support SF5.0 (#29)
    * Drop 3.4 to support 5.0
    
    * Make sure build is green

--- DependencyInjection/HappyrAuth0Extension.php
+++ DependencyInjection/HappyrAuth0Extension.php
@@ -2,11 +2,11 @@
 
 namespace Happyr\Auth0Bundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
 /**
  * This is the class that loads and manages your bundle configuration.
