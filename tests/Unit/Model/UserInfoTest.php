3f01edf76c571098ad6ab9798049f7a72ddcffae
Nyholm <tobias.nyholm@gmail.com>
2020-12-01 11:03:10 +0100

    Added a small test

---
+++ tests/Unit/Model/UserInfoTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Happyr\Auth0Bundle\Tests\Unit\Model;
+
+use Happyr\Auth0Bundle\Model\UserInfo;
+use PHPUnit\Framework\TestCase;
+
+class UserInfoTest extends TestCase
+{
+    public function testGetUserId()
+    {
+        $user = UserInfo::create(['user_id'=>'auth0|abc']);
+        $this->assertEquals('auth0|abc', $user->getUserId());
+    }
+}
