3ce6be4569fb2ea83ee5bf873cb4346726d10806
Jérémy Leherpeur <amenophis@leherpeur.net>
2021-08-02 21:03:18 +0200

    Remove phpdocumentor/reflection-docblock

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -3,8 +3,6 @@
 namespace Happyr\Auth0Bundle\DependencyInjection;
 
 use Auth0\SDK\Configuration\SdkConfiguration;
-use phpDocumentor\Reflection\DocBlock\Tags\Param;
-use phpDocumentor\Reflection\DocBlockFactory;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -48,47 +46,25 @@ class Configuration implements ConfigurationInterface
 
     private function addAuth0SdkConfiguration(ArrayNodeDefinition $rootNode): void
     {
-        $sdkConfigurationRefClass = new \ReflectionClass(SdkConfiguration::class);
-        $constructor = $sdkConfigurationRefClass->getConstructor();
-
-        $docBlockFactory = DocBlockFactory::createInstance();
-        $block = $docBlockFactory->create($constructor->getDocComment());
-        $params = $block->getTagsByName('param');
-
         $sdkNode = $rootNode
             ->children()
                 ->arrayNode('sdk')
                     ->children();
 
-        /** @var Param $param */
-        foreach ($params as $param) {
-            if ($param->getVariableName() === 'configuration') {
+
+        $sdkConfigurationRefClass = new \ReflectionClass(SdkConfiguration::class);
+        $constructor = $sdkConfigurationRefClass->getConstructor();
+
+        foreach ($constructor->getParameters() as $parameter) {
+            if ('configuration' === $parameter->getName()) {
                 continue;
             }
 
+            $defaultValue = $parameter->isOptional() ? $parameter->getDefaultValue() : null;
+
             $sdkNode
-                ->scalarNode($param->getVariableName())
-                ->defaultValue($this->getDefaultValue($param->getVariableName(), $constructor->getParameters()))
-                ->info($param->getDescription());
+                ->scalarNode($parameter->getName())
+                ->defaultValue($defaultValue);
         }
-    }
-
-    /**
-     * @param string $parameterName
-     * @param array<\ReflectionParameter> $parameters
-     */
-    private function getDefaultValue(string $parameterName, array $parameters): mixed
-    {
-        foreach ($parameters as $parameter) {
-            if ($parameter->getName() === $parameterName) {
-                if (!$parameter->isOptional()) {
-                    return null;
-                }
-
-                return $parameter->getDefaultValue();
-            }
-        }
-
-        return null;
     }
 }
