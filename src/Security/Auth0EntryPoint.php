055aed28bd594af87fdd541a02cce0d24f083e4a
Nyholm <tobias.nyholm@gmail.com>
2021-08-14 18:11:04 -0700

    cs

--- src/Security/Auth0EntryPoint.php
+++ src/Security/Auth0EntryPoint.php
@@ -8,7 +8,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
-use Symfony\Component\Security\Http\HttpUtils;
 
 /**
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
